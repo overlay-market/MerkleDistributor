b409c7342d57ee55dff6cb2e5d44fb524e6a7776
c-note <dongustavo20@gmail.com>
2023-01-24 12:37:32 +0100

    added files

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require('dotenv').config()
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 5000,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      settings: {
+        debug: {
+          revertStrings: 'debug',
+        },
+      },
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`, // or any other JSON-RPC provider
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`, // or any other JSON-RPC provider
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+}
