e08398d6ed34b5b1eb8be61a5ad8487fb4b2d235
c-note <dongustavo20@gmail.com>
2023-03-04 20:21:37 +0100

    updated files

--- scripts/deployMerkleDistributor.ts
+++ scripts/deployMerkleDistributor.ts
@@ -6,7 +6,7 @@ import { tree } from './Tree'
 
 async function main() {
   const root = tree()
-  const endTime = 1678296600
+  const endTime = 1680629400
 
   const TokenArb = '0x4305C4Bc521B052F17d389c2Fe9d37caBeB70d54'
   const OvlFoundationAdddress = '0xBC443021E85837Ee92dAf1378a2209A2c23a0062'
@@ -29,5 +29,5 @@ main()
     process.exit(1)
   })
 
-// npx hardhat run --network goerli scripts/deployMerkleDistributor.ts
-// npx hardhat verify --network goerli 0x3C28Bb293A1f7AA17A29C570e023042346444846 Demo DM 100000000000000000000
+// npx hardhat run --network arbitrum_mainnet scripts/deployMerkleDistributor.ts
+// npx hardhat verify --network arbitrum_mainnet 0x5a0B95E113B6713eD3dE69E47af4a946F9538b8a 0x4305C4Bc521B052F17d389c2Fe9d37caBeB70d54 0xfeed7ee59645916680aff6cfa31d3154bf4529f0c3b2253d48e65315d96d58a5 1680629400
